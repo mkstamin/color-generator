194dc3dded367ee25d44396495e7dafedddeb123
mkstamin <mkstamincse@gmail.com>
2021-06-01 12:00:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Values from 'values.js';
+import SingleColor from './components/SingleColor';
 
 function App() {
+
+  const [color, setColor] = useState('');
+const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#746a4f').all(9));
+
+
+  const handelSubmit = (e)=>{
+    e.preventDefault();
+    //all(10) here 10 = weight
+    try {
+      const colors = new Values(color).all(9)
+      console.log(colors);
+      setList(colors);
+    } catch (err) {
+      setError(true)
+      console.log(err);
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>  
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handelSubmit}>
+        <input 
+          type="text" 
+          className={`${error ? 'error' : null}`} 
+          placeholder="#746a4f"
+          onChange={(e)=>setColor(e.target.value)}
+        />
+        <button type="submit" className="btn">submit</button>
+      </form>
+    </section>
+    <section className="colors">
+      {list.map((color,index)=>{
+        return(
+          <SingleColor 
+            key={index}
+            {...color}
+            index={index}
+            hexColor={color.hex}
+          />
+        )
+      })}
+    </section>
+  </>  
   );
 }
 
-export default App;
+export default App
---
+++ src/components/SingleColor.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+const SingleColor = ({rgb, weight, index, hexColor}) => {
+ 
+  const [alert, setAlert] = useState(false);
+ 
+ 
+  const bcg = rgb.join(',')
+  const hexValue =`#${hexColor}`
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false)
+    }, 3000);
+    return () => {
+      clearTimeout(timeOut)
+    };
+  }, [alert]);
+
+  return (
+    <article 
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{backgroundColor: `rgb(${bcg})`}}
+      onClick={()=>{
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
+    >
+      <p className="percent-value">{weight}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied ✔</p>}
+    </article>
+  )
+}
+
+export default SingleColor
